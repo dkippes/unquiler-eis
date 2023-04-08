30fdec42d6f8daec0cea1cb810215782daf7a545
dkippes <kippes.diego@gmail.com>
2023-04-08 01:32:58 -0300

    rollback de test integradores e implementaciones innecesarias

--- be-unquiler/src/main/kotlin/com/unquiler/beunquiler/controllers/PingController.kt
+++ be-unquiler/src/main/kotlin/com/unquiler/beunquiler/controllers/PingController.kt
@@ -1,18 +1,10 @@
 package com.unquiler.beunquiler.controllers
 
-import com.unquiler.beunquiler.repositories.entities.User
-import com.unquiler.beunquiler.services.UserService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PingController(var userService: UserService) {
-
+class PingController {
     @GetMapping("/ping")
     fun ping() = "pong"
-
-    @GetMapping("/test")
-    fun test() {
-        userService.register(User())
-    }
 }
--- be-unquiler/src/main/kotlin/com/unquiler/beunquiler/services/UserService.kt
+++
@@ -1,7 +0,0 @@
-package com.unquiler.beunquiler.services
-
-import com.unquiler.beunquiler.repositories.entities.User
-
-interface UserService {
-    fun register(user: User): User
-}
