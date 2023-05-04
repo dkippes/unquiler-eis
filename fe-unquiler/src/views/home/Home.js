090fbc423d06fa607f0ad6cb7629cd2c29b129c2
JcOzores97 <juanozores97@gmail.com>
2023-05-03 23:37:21 -0300

    search bar layout

--- fe-unquiler/src/views/home/Home.js
+++ fe-unquiler/src/views/home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Heading, Text, VStack } from '@chakra-ui/react';
+import {Flex, Heading, IconButton, Input, Spinner, Text, VStack} from '@chakra-ui/react';
 import Header from '../../components/Header';
 import Layout from '../components/Layout';
+import {SearchIcon} from "@chakra-ui/icons";
+
 
 const Home = () => {
   return (
     <Layout>
-      <Header />
+          <Header>
+              <Flex>
+                  <Input bg="whitesmoke" placeholder='Buscar...' focusBorderColor="grey" borderColor="grey"/>
+                  <IconButton ml="10px" aria-label='Buscar club' icon={<SearchIcon />}   borderColor="blackAlpha.100" spinner={<Spinner/>}/>
+              </Flex>
+          </Header>
       <VStack color="brand.200" p={6} align={'flex-start'}>
         <Heading size="4xl"> Bienvenido a UNQuiler!</Heading>
         <Text fontSize={'xl'}>
